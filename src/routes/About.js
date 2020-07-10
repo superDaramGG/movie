6beb83a97f5f11ca59e16d153b62b024ac4433e2
Yeseo <superior817@naver.com>
2020-07-10 21:54:30 +0900

    수정 코드

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div class="about__container">
+      <span>영화를 사랑하는 모임</span>
+    </div>
+  );
+}
+
+export default About;
